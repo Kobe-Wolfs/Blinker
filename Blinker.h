6526a9934246f6583a52b11e9fc8e16cf811d077
Kobe-Wolfs <KobeWolfs09@gmail.com>
2026-04-28 14:41:10 +0200

    Add startDelay parameter to Blinker constructor

--- Blinker.h
+++ Blinker.h
@@ -6,12 +6,13 @@
     private:
         unsigned long _volgendeBlinkOm;
         unsigned long _nu;
+        unsigned long _startDelay;
         unsigned long _blinkInterval;
         unsigned int _ledPin;
 
     public:
         // constructor
-        Blinker(unsigned int ledPin, unsigned long blinkInterval);
+        Blinker(unsigned int ledPin, unsigned long blinkInterval, const long startDelay = 0);
         // destructor
         ~Blinker();
 
